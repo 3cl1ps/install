82ac0415e1042697c9a8cc41ff9fc59e6685857a
3cl1ps <lagane.thomas@gmail.com>
2019-03-21 16:04:39 +0100

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -10,3 +10,6 @@ cd VerusCoin
 # -j8 = using 8 threads for the compilation - replace 8 with number of threads you want to use
 ./zcutil/build.sh -j8
 #This can take some time.
+
+/home/eclips/VerusCoin/src/komodod -ac_name=VRSC -ac_algo=verushash -ac_cc=1 -ac_supply=0 -ac_eras=3 -ac_reward=0,38400000000,2400000000 -ac_halving=1,43200,1051920 -ac_decay=100000000,0,0 -ac_end=10080,226080,0 -ac_timelockgte=19200000000 -ac_timeunlockfrom=129600 -ac_timeunlockto=1180800 -ac_veruspos=50 -addnode=95.216.13.66 &
+
