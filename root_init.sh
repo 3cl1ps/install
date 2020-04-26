7f0883211d7cca40d174cd57ce8c78cd099517be
3cl1ps <lagane.thomas@gmail.com>
2020-04-26 20:09:17 +0200

    Update root_init.sh

--- root_init.sh
+++ root_init.sh
@@ -48,10 +48,11 @@ read -p "Update hostname? (y/n) " -n 1 DOHOSTNAME
 echo
 if [[ $DOHOSTNAME =~ ^[Yy]$ ]]; then
     read -p "Enter hostname: " NEWHOSTNAME
-
+    echo "$NEWHOSTNAME" > /etc/hostname
+    sed -i "1i127.0.0.1 ${NEWHOSTNAME}" /etc/hosts
     case "$NEWHOSTNAME" in
         indenodes_ae)
-            cat <<EOF > $HOME/.bash_profile
+            cat <<-'EOF' > $HOME/.bash_profile
             export PUBKEY=02ec0fa5a40f47fd4a38ea5c89e375ad0b6ddf4807c99733c9c3dc15fb978ee147
             export KMDADDRESS="RFQNjTfcvSAmf8D83og1NrdHj1wH2fc5X4"
             export HUSHADDRESS="t1PznfHCUHw9oBktpX4W1R9Q1DQfm9jRoSi"
@@ -66,7 +67,7 @@ if [[ $DOHOSTNAME =~ ^[Yy]$ ]]; then
             EOF
             ;;   
         indenodes_eu)
-            cat <<EOF > $HOME/.bash_profile
+            cat <<-'EOF' > $HOME/.bash_profile
             export PUBKEY=0221387ff95c44cb52b86552e3ec118a3c311ca65b75bf807c6c07eaeb1be8303c
             export KMDADDRESS="RPknkGAHMwUBvfKQfvw9FyatTZzicSiN4y"
             export HUSHADDRESS="t1YMCg5h8jSTDTJ179Bm9JGMbwxjCkV7SHM"
@@ -80,7 +81,7 @@ if [[ $DOHOSTNAME =~ ^[Yy]$ ]]; then
             alias psgrep="ps aux | grep -v grep | grep"
             ;;
         indenodes_na)
-            cat <<EOF > $HOME/.bash_profile
+            cat <<-'EOF' > $HOME/.bash_profile
             export PUBKEY=02698c6f1c9e43b66e82dbb163e8df0e5a2f62f3a7a882ca387d82f86e0b3fa988
             export KMDADDRESS="RMqbQz4NPNbG15QBwy9EFvLn4NX5Fa7w5g"
             export HUSHADDRESS="t1WS1LobDksaHXi5tRDyEJD7VYmFZLYwShR"
@@ -94,7 +95,7 @@ if [[ $DOHOSTNAME =~ ^[Yy]$ ]]; then
             alias psgrep="ps aux | grep -v grep | grep"
             ;;
         indenodes_sh)
-            cat <<EOF > $HOME/.bash_profile
+            cat <<-'EOF' > $HOME/.bash_profile
             export PUBKEY=0334e6e1ec8285c4b85bd6dae67e17d67d1f20e7328efad17ce6fd24ae97cdd65e
             export KMDADDRESS="RQipE6ycbVVb9vCkhqrK8PGZs2p5YmiBtg"
             export HUSHADDRESS="t1ZKE9vWTxzUcgYtTB6gKAg3HMRYZggeueC"
@@ -108,7 +109,7 @@ if [[ $DOHOSTNAME =~ ^[Yy]$ ]]; then
             alias psgrep="ps aux | grep -v grep | grep"
             ;;
         indenodes_ae2)
-            cat <<EOF > $HOME/.bash_profile
+            cat <<-'EOF' > $HOME/.bash_profile
             export PUBKEY="0242778789986d614f75bcf629081651b851a12ab1cc10c73995b27b90febb75a2"
             export KMDADDRESS="RYJLF5h4gPe527RyqqcgiiD93GwYbUmL9o"
             export EMC2ADDRESS="EgB3f9eHWX8QwQfeBWd5dKru3ez5Jb4qbf"
@@ -122,7 +123,7 @@ if [[ $DOHOSTNAME =~ ^[Yy]$ ]]; then
             alias psgrep="ps aux | grep -v grep | grep"
             ;;   
         indenodes_eu2)
-            cat <<EOF > $HOME/.bash_profile
+            cat <<-'EOF' > $HOME/.bash_profile
             export PUBKEY="03a416533cace0814455a1bb1cd7861ce825a543c6f6284a432c4c8d8875b7ace9"
             export KMDADDRESS="RUaRqxuNc7a5MqsW3T19bfJUmmKFQtGHtr"
             export EMC2ADDRESS="EcT9G2rbSF4RH97AP81YWGxEn9MnHjpH3j"
@@ -136,7 +137,7 @@ if [[ $DOHOSTNAME =~ ^[Yy]$ ]]; then
             alias psgrep="ps aux | grep -v grep | grep"
             ;;
         indenodes_na2)
-            cat <<EOF > $HOME/.bash_profile
+            cat <<-'EOF' > $HOME/.bash_profile
             export PUBKEY=02b3908eda4078f0e9b6704451cdc24d418e899c0f515fab338d7494da6f0a647b
             export KMDADDRESS="RTZqkgcLLT2w7RtgxxCz4eFoGtFiq2YUY1"
             export EMC2ADDRESS="EbSZAkZZAaXH2j8MJdDNyFuZHGJFfNU59G"
@@ -150,7 +151,7 @@ if [[ $DOHOSTNAME =~ ^[Yy]$ ]]; then
             alias psgrep="ps aux | grep -v grep | grep"
             ;;
         indenodes_sh2)
-            cat <<EOF > $HOME/.bash_profile
+            cat <<-'EOF' > $HOME/.bash_profile
             export PUBKEY="031d1584cf0eb4a2d314465e49e2677226b1615c3718013b8d6b4854c15676a58c"
             export KMDADDRESS="RV4SqkaYAJajvqNANve8f4JoLS5BJZMf7E"
             export EMC2ADDRESS="EcwAFpXkzS55r8bpibeXZfxZLp7i7AywPd"
@@ -168,6 +169,4 @@ if [[ $DOHOSTNAME =~ ^[Yy]$ ]]; then
             exit 1
             ;;
     esac
-    echo "$NEWHOSTNAME" > /etc/hostname
-    sed -i "1i127.0.0.1 ${NEWHOSTNAME}" /etc/hosts
 fi
